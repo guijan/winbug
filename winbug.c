b94152afd983b3f702deea082610882dd8c81187
Guilherme Janczak <guilherme.janczak@yandex.com>
2022-10-10 11:04:20 +0000

    simplify the test

--- winbug.c
+++ winbug.c
@@ -1,7 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /* null_handler: Windows invalid parameter handler that does nothing. */
 static void
@@ -16,18 +15,15 @@ null_handler(wchar_t const* const unused0, wchar_t const* const unused1,
 }
 
 int
-main(int argc, char *argv[])
+main(void)
 {
-	_invalid_parameter_handler handler;
-	int new_max;
-	handler = _set_invalid_parameter_handler(null_handler);
+	_set_invalid_parameter_handler(null_handler);
 
-	/* If the program was given exactly one argument and the argument
-	 * matches the string "hang", try to set the fd limit to 10000.
-	 * Otherwise, set it to 2048. 10000 triggers a Windows bug and causes
-	 * the program to hang, 2048 doesn't.
+	/*
+	 * Setting the stdio limit over Windows' maximum causes the the program
+	 * to hang. As of Windows 10 and Windows Server 2022, this limit is
+	 * 8192.
 	 */
-	new_max = (argc == 2 && strcmp(argv[1], "hang") == 0) ? 10000 : 2048;
-	_setmaxstdio(new_max);
+	_setmaxstdio(10000);
 	return 0;
 }
